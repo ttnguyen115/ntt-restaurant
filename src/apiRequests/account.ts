8c1eb38ea2d2a00f6d8ef7c28451cb864389fcb7
ttnguyen115 <thanhtrung1012000@gmail.com>
2025-01-23 12:25:00 +0700

    add api requests and hooks for accounts

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -3,9 +3,12 @@ import { ApiRoutes } from '@/constants';
 import { http } from '@/lib';
 
 import type {
+    AccountListResType,
     AccountResType,
     ChangePasswordBodyType,
     ChangePasswordResType,
+    CreateEmployeeAccountBodyType,
+    UpdateEmployeeAccountBodyType,
     UpdateMeBodyType,
 } from '@/schemaValidations';
 
@@ -22,16 +25,27 @@ const accountApiRequest = {
     updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>(ApiRoutes.ME, body),
 
     sChangePassword: (accessToken: string, body: ChangePasswordBodyType) =>
-        http.put<ChangePasswordResType>(ApiRoutes.SERVER_CHANGE_PASSWORD, body, {
+        http.put<ChangePasswordResType>(ApiRoutes.SERVER_API_CHANGE_PASSWORD, body, {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
             },
         }),
 
     changePassword: (body: ChangePasswordBodyType) =>
-        http.put<ChangePasswordResType>(ApiRoutes.CLIENT_CHANGE_PASSWORD, body, {
+        http.put<ChangePasswordResType>(ApiRoutes.CLIENT_API_CHANGE_PASSWORD, body, {
             baseUrl: '',
         }),
+
+    getAllAccounts: () => http.get<AccountListResType>(ApiRoutes.API_ACCOUNTS),
+
+    getEmployee: (id: number) => http.get<AccountResType>(`${ApiRoutes.SERVER_API_ACCOUNT_DETAIL}/${id}`),
+
+    addEmployee: (body: CreateEmployeeAccountBodyType) => http.post<AccountResType>(ApiRoutes.API_ACCOUNTS, body),
+
+    updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) =>
+        http.put<AccountResType>(`${ApiRoutes.SERVER_API_ACCOUNT_DETAIL}/${id}`, body),
+
+    deleteEmployee: (id: number) => http.delete<AccountResType>(`${ApiRoutes.SERVER_API_ACCOUNT_DETAIL}/${id}`),
 };
 
 export default accountApiRequest;
--- src/hooks/queries/useAccount.ts
+++ src/hooks/queries/useAccount.ts
@@ -1,6 +1,8 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { accountApiRequest } from '@/apiRequests';
+
+import { UpdateEmployeeAccountBodyType } from '@/schemaValidations';
 
 export const useMyAccount = () => {
     return useQuery({
@@ -18,5 +20,60 @@ export const useMyAccountMutation = () => {
 export const useMyPasswordMutation = () => {
     return useMutation({
         mutationFn: accountApiRequest.changePassword,
+    });
+};
+
+export const useGetAllAccounts = () => {
+    return useQuery({
+        queryKey: ['accounts'],
+        queryFn: accountApiRequest.getAllAccounts,
+    });
+};
+
+export const useGetAccount = ({ id }: { id: number }) => {
+    return useQuery({
+        queryKey: ['accounts', id],
+        queryFn: () => accountApiRequest.getEmployee(id),
+    });
+};
+
+export const useAddAccount = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: accountApiRequest.addEmployee,
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['accounts'],
+            });
+        },
+    });
+};
+
+type UseUpdateAccountMutationParams = UpdateEmployeeAccountBodyType & { id: number };
+
+export const useUpdateAccount = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({ id, ...body }: UseUpdateAccountMutationParams) => accountApiRequest.updateEmployee(id, body),
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['accounts'],
+            });
+        },
+    });
+};
+
+export const useDeleteAccount = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: accountApiRequest.deleteEmployee,
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['accounts'],
+            });
+        },
     });
 };
