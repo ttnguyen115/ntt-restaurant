cf6bb650371f1cf7f75f1992a9fa9657db710e47
ttnguyen115 <thanhtrung1012000@gmail.com>
2025-03-07 21:58:58 +0700

    add bundle analyzer for app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import NextBundleAnalyzer from '@next/bundle-analyzer';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/lib/i18n/request.ts');
+
+const withBundleAnalyzer = NextBundleAnalyzer({
+    enabled: (process.env.ANALYZE = 'true'),
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,4 +24,4 @@ const nextConfig = {
     },
 };
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(withBundleAnalyzer(nextConfig));
